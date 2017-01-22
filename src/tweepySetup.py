513d2eebfd091590a1c28d6f83dc1c233a510bc1
Stephane Lin <lins.stephane@gmail.com>
2017-01-21 23:48:33 -0500

    Query for YP hashtag

--- src/tweepySetup.py
+++ src/tweepySetup.py
@@ -7,6 +7,7 @@ auth.set_access_token("63926897-UyXhEiGXD3mhnRbUvaeEOsayiYTykCEWACyYBrFoe",
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+# public_tweets = api.home_timeline()
+public_tweets = api.search(q="%23askYP", count=100)
 for tweet in public_tweets:
-    print (tweet.text)
+    print("User: {}, tweet: {}".format(tweet.user.screen_name, tweet.text))
