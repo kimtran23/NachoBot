2a74e94d8a7f6372f8e817446403142b09c97f09
Kimberly <kimberly.tran23@gmail.com>
2017-01-22 09:04:50 -0500

    Change api key

--- src/textparser.py
+++ src/textparser.py
@@ -2,13 +2,13 @@ import textrazor
 import tweepySetup
 import string
 
-textrazor.api_key = "206536a24ac40e9b546a305489d9c83837c9200a269919d03db61b09"
+textrazor.api_key = "d1103f73c05ca5009594951423c82b86c45df568afebc6df42f20769"
 
 sentenceList = tweepySetup.tweetMsg()
 exclude = set(string.punctuation)
 for s in sentenceList:
     sentence = ''.join(ch for ch in s if ch not in exclude)
-    if len(sentence)>1:
+    if len(sentence) > 1:
         responselist = sentence.split()
 
         wordlist = []
@@ -21,23 +21,23 @@ for s in sentenceList:
         specialword = []
 
         for idx, word in enumerate(wordlist):
-            if word == "VB" or word=="VBP" or word=="TO" or word=="DT" or word=="JJ" or word=="JJS":
+            if word == "VB" or word == "VBP" or word == "TO" or word == "DT" or word == "JJ" or word == "JJS":
                 flag.append(idx)
 
         lastind = 0
         print(len(flag))
         print(len(responselist))
-        if len(flag)!=0:
+        if len(flag) != 0:
             for i in flag:
-                 if wordlist[i+1] == "NN" or wordlist[i+1]=="NNS" or wordlist[i+1]=="NNP" or wordlist[i+1]=="NNPS":
-                    #  print(i)
-                     specialword.append(responselist[i+1])
-                     lastind = i
-                     break
+                if wordlist[i + 1] == "NN" or wordlist[i + 1] == "NNS" or wordlist[i + 1] == "NNP" or wordlist[i + 1] == "NNPS":
+                   #  print(i)
+                    specialword.append(responselist[i + 1])
+                    lastind = i
+                    break
 
             try:
-                if wordlist[lastind+2]=="NN" or wordlist[lastind+2]=="NNS" or wordlist[lastind+2]=="NNP" or wordlist[lastind+2]=="NNPS":
-                    specialword.append(responselist[lastind+2])
+                if wordlist[lastind + 2] == "NN" or wordlist[lastind + 2] == "NNS" or wordlist[lastind + 2] == "NNP" or wordlist[lastind + 2] == "NNPS":
+                    specialword.append(responselist[lastind + 2])
             except IndexError:
                 pass
         print (sentence)
