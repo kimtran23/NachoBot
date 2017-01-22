e6064d0f558311b2a80ccff64eef3c6e649342d3
Victor Uong <victor.uong@mail.mcgill.ca>
2017-01-22 08:50:45 -0500

    for stephane

--- src/textparser.py
+++ src/textparser.py
@@ -1,53 +1,61 @@
 import textrazor
+import tweepySetup
+import string
 
 textrazor.api_key = "206536a24ac40e9b546a305489d9c83837c9200a269919d03db61b09"
 
-sentence = "Where can I get Louis Vuitton shoes."
-responselist = sentence.split()
-output = ""
-r = responselist[-1]
-for c in r:
-    if "." in c:
-        tmp = " "+"."
-        output = output + tmp
-    elif "?" in c:
-        tmp = " "+"?"
-        output = output + tmp
-    elif "!" in c:
-        tmp = " "+"!"
-        output = output + tmp
-    else:
-        output = output + c
+sentenceList = tweepySetup.tweetMsg()
+exclude = set(string.punctuation)
+for s in sentenceList:
+    sentence = ''.join(ch for ch in s if ch not in exclude)
+    responselist = sentence.split()
+    # output = ""
+    # lastWord = responselist[-1]
+    # for char in lastWord:
+    #     if "." in char:
+    #         tmp = " "+"."
+    #         output = output + tmp
+    #     elif "?" in char:
+    #         tmp = " "+"?"
+    #         output = output + tmp
+    #     elif "!" in char:
+    #         tmp = " "+"!"
+    #         output = output + tmp
+    #     else:
+    #         output = output + char
+    # output = output.split()
+    # responselist[-1] = output[0]
+    # responselist.append(output[-1])
 
-output = output.split()
-responselist[-1] = output[0]
-responselist.append(output[-1])
+    wordlist = []
+    client = textrazor.TextRazor(extractors=["words"])
+    response = client.analyze(sentence)
+    for word in response.words():
+        wordlist.append(word.part_of_speech)
 
-client = textrazor.TextRazor(extractors=["words"])
-response = client.analyze(sentence)
 
-wordlist = list()
-print (responselist)
-for word in response.words():
-    wordlist.append(word.part_of_speech)
-flag = list()
-print(wordlist)
+    flag = []
+    specialword = []
 
-for idx, word in enumerate(wordlist):
-    if word == "VB" or word=="VBP" or word=="TO" or word=="DT" or word=="JJ" or word=="JJS":
-        flag.append(idx)
-specialword = list()
-lastind = 0
-for i in flag:
-     if wordlist[i+1] == "NN" or wordlist[i+1]=="NNS" or wordlist[i+1]=="NNP" or wordlist[i+1]=="NNPS":
-         specialword.append(responselist[i+1])
-         lastind = i
-         break
+    for idx, word in enumerate(wordlist):
+        if word == "VB" or word=="VBP" or word=="TO" or word=="DT" or word=="JJ" or word=="JJS":
+            flag.append(idx)
 
-try:
-    if wordlist[lastind+2]=="NN" or wordlist[lastind+2]=="NNS" or wordlist[lastind+2]=="NNP" or wordlist[lastind+2]=="NNPS":
-        specialword.append(responselist[lastind+2])
-except IndexError:
-    pass
+    lastind = 0
+    print(len(flag))
+    print(len(responselist))
+    if len(flag)!=0:
+        for i in flag:
+             if wordlist[i+1] == "NN" or wordlist[i+1]=="NNS" or wordlist[i+1]=="NNP" or wordlist[i+1]=="NNPS":
+                #  print(i)
+                 specialword.append(responselist[i+1])
+                 lastind = i
+                 break
 
-print(specialword)
+        try:
+            if wordlist[lastind+2]=="NN" or wordlist[lastind+2]=="NNS" or wordlist[lastind+2]=="NNP" or wordlist[lastind+2]=="NNPS":
+                specialword.append(responselist[lastind+2])
+        except IndexError:
+            pass
+    print (sentence)
+    print(specialword)
--- src/tweepySetup.py
+++ src/tweepySetup.py
@@ -1,29 +1,31 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("P59fg00MtbVNnd0YR9Zq1TZJv",
-                           "mJxrGLjiqcDREuI5ow3iBNuEyUjDQNsQgqm6jXJKL2BibuBXtD")
-auth.set_access_token("63926897-UyXhEiGXD3mhnRbUvaeEOsayiYTykCEWACyYBrFoe",
-                      "cV1tEVIXwUF9kdUfipekTTqYIWDSMjY3UFzxpxa8k7MSq")
+def tweetMsg():
+    auth = tweepy.OAuthHandler("P59fg00MtbVNnd0YR9Zq1TZJv",
+                               "mJxrGLjiqcDREuI5ow3iBNuEyUjDQNsQgqm6jXJKL2BibuBXtD")
+    auth.set_access_token("63926897-UyXhEiGXD3mhnRbUvaeEOsayiYTykCEWACyYBrFoe",
+                          "cV1tEVIXwUF9kdUfipekTTqYIWDSMjY3UFzxpxa8k7MSq")
 
-api = tweepy.API(auth)
+    api = tweepy.API(auth)
 
-# public_tweets = api.home_timeline()
-public_tweets = api.search(q="%23askYP", count=100)
-for tweet in public_tweets:
-    if tweet.place is not None:
-        location = tweet.place.name
-    else:
-        location = "Montreal"
-
-    print("User: {}, tweet: {}, city: {}".format(
-        tweet.user.screen_name, tweet.text, location))
-
-    words = tweet.text.split()
-    tweetMsg = ""
-    for word in words:
-        if ("#" in word or "@" in word or "RT" in word):
-            continue
+    tweetlist = list()
+    # public_tweets = api.home_timeline()
+    public_tweets = api.search(q="%23askYP", count=100)
+    for tweet in public_tweets:
+        if tweet.place is not None:
+            location = tweet.place.name
         else:
-            tweetMsg += word + " "
+            location = "Montreal"
 
-    print(tweetMsg)
+        print("User: {}, tweet: {}, city: {}".format(
+            tweet.user.screen_name, tweet.text, location))
+
+        words = tweet.text.split()
+        tweetMsg = ""
+        for word in words:
+            if ("#" in word or "@" in word or "RT" in word):
+                continue
+            else:
+                tweetMsg += word + " "
+        tweetlist.append(tweetMsg)
+    return tweetlist
